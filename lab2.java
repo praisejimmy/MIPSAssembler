37c868dca40becd939fd69900dd76f1ba98e142d
Ryan Myers <ryanmyers@Ryans-MacBook-Pro-2.local>
2018-10-12 13:30:17 -0700

    fixed indexing of labels

--- lab2.java
+++ lab2.java
@@ -16,28 +16,28 @@ public class lab2 {
         int counter = 0x00;
         while (sc.hasNextLine()) {
             String code_ln = sc.nextLine().trim();
-            
+
             int comment_loc = code_ln.indexOf('#');
             if (comment_loc != -1)
                 code_ln = code_ln.substring(0, comment_loc).trim();
-        
+
             int colon_loc = code_ln.indexOf(':');
             if (colon_loc != -1){
                 Label new_label = new Label(code_ln.substring(0,colon_loc), -1);
                 labels.add(new_label);
                 code_ln = code_ln.substring(colon_loc).trim();
-            }            
-            
+            }
+
             if (code_ln.length() > 1){//more than just a colon. real code
                 //loop through and set address
                 int index = labels.size()-1;
-                while(labels.get(index).getAddress() == -1){
+                while(index >= 0 && labels.get(index).getAddress() == -1){
                     labels.get(index).setAddress(counter);
                     index--;
                 }
 
                 //make instruction
-                                
+
 
                 //increment address
                 counter++;
